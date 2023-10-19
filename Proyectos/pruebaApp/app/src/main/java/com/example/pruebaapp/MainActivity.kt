bbd3e54f8241adca636f5a52574940c078c672e4
velasco182 <ruben.velasco0@misena.edu.co>
2023-10-19 18:12:16 -0500

    proyecto1 Android 19/10/2023

--- Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
+++ Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.pruebaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cadena : String;
+    var cadena2 = "";
+    val miConst = "Esto es una cadena.";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_1).setOnClickListener{
+
+            //Toast.makeText(this, "Hola Mundo", Toast.LENGTH_SHORT).show();
+
+            var input = findViewById<TextInputEditText>(R.id.textInputEditText).text;
+            findViewById<TextView>(R.id.textView).setText(input);
+
+
+        }
     }
+
+
 }
