6d1bc2006917d53feef8444ea08d65f049c74c83
velasco182 <ruben.velasco0@misena.edu.co>
2023-11-03 18:32:30 -0500

    Schemaas avanzado

--- Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
+++ Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.pruebaapp.data.SharedPreferencesManager
 import com.example.pruebaapp.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var cadena : String;
     var cadena2 = "";
     val miConst = "Esto es una cadena.";
+    private lateinit var sharedPre: SharedPreferencesManager
 
     ///Binding, extendemos binding con ":" del activity, aparece automáticamente la clase
     private lateinit var binding: ActivityMainBinding
@@ -29,9 +31,9 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        Toast.makeText(this, "Hola Mundo", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hola Mundo", Toast.LENGTH_SHORT).show()
 
-        val valor = intent.getStringExtra("user")
+        val valor = sharedPre.getUser()
         //intent.getBooleanExtra() Para pasar booleanos
 
         val textView = binding.textView.setText(valor)
